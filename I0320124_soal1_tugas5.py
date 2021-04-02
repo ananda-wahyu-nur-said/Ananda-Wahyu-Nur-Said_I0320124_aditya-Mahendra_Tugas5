673a7ccb0750d091c10ab2a77b347b9ae7438a26
ananda-wahyu-nur-said <80508707+ananda-wahyu-nur-said@users.noreply.github.com>
2021-04-02 22:07:14 +0700

    Add files via upload
    tugas 5

---
+++ I0320124_soal1_tugas5.py
@@ -0,0 +1,6 @@
+nama = str(input('Nama anda siapa?'))
+jenis_kelamin = str(input('Apa jenis kelamin anda! (L/P)'))
+if jenis_kelamin == 'L':
+    print('Selamat datang, Tuan', nama,'!')
+else :
+    print('Selamat datang, Nyonya', nama,'!')
